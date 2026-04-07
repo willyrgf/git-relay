c4f38c2631a70bf505a5d9d0c008eeb326c6f54b
Willy Romão <willyr.goncalves@gmail.com>
2026-04-07 15:18:53 +0800

    wire rfc proof profiles into flake checks

--- flake.nix
+++ flake.nix
@@ -98,6 +98,62 @@
             touch $out
           '';
 
+          mkRfcProofCheck = { checkName, testFilter }:
+            pkgs.rustPlatform.buildRustPackage {
+              pname = checkName;
+              version = "0.1.0";
+              src = self;
+              cargoLock.lockFile = ./Cargo.lock;
+              nativeCheckInputs = [
+                pkgs.git
+                pkgs.nix
+                pkgs.openssh
+                pkgs.python3
+              ];
+              doCheck = true;
+              checkPhase = ''
+                runHook preCheck
+
+                export HOME="$TMPDIR/home"
+                mkdir -p "$HOME"
+
+                export GIT_RELAY_PROOF_ENABLE=1
+                export GIT_RELAY_PROOF_GATE_MODE=1
+                export GIT_RELAY_PROOF_BIN_GIT_RELAY=${gitRelay}/bin/git-relay
+                export GIT_RELAY_PROOF_BIN_GIT_RELAYD=${gitRelay}/bin/git-relayd
+                export GIT_RELAY_PROOF_BIN_GIT_RELAY_INSTALL_HOOKS=${gitRelay}/bin/git-relay-install-hooks
+                export GIT_RELAY_PROOF_BIN_GIT_RELAY_SSH_FORCE_COMMAND=${gitRelay}/bin/git-relay-ssh-force-command
+                export GIT_RELAY_PROOF_SSHD_BIN=${pkgs.openssh}/bin/sshd
+                export GIT_RELAY_PROOF_SSH_BIN=${pkgs.openssh}/bin/ssh
+                export GIT_RELAY_PROOF_SSH_KEYGEN_BIN=${pkgs.openssh}/bin/ssh-keygen
+                export GIT_RELAY_PROOF_PYTHON_BIN=${pkgs.python3}/bin/python3
+                export GIT_RELAY_PROOF_GIT_HTTP_BACKEND_BIN=${pkgs.git}/libexec/git-core/git-http-backend
+
+                cargo test --locked --offline --test rfc_proof_e2e ${testFilter} -- --test-threads=1
+
+                runHook postCheck
+              '';
+              installPhase = ''
+                mkdir -p $out
+                touch $out/passed
+              '';
+            };
+
+          rfcProofFast = mkRfcProofCheck {
+            checkName = "rfc-proof-e2e-fast";
+            testFilter = "proof_e2e_fast_profile_runs_required_cases";
+          };
+
+          rfcProofFull = mkRfcProofCheck {
+            checkName = "rfc-proof-e2e-full";
+            testFilter = "proof_e2e_full_profile_reruns_and_hashes";
+          };
+
+          rfcProofProviderAdmission = mkRfcProofCheck {
+            checkName = "rfc-proof-provider-admission";
+            testFilter = "proof_e2e_provider_admission";
+          };
+
           mkApp = package: binaryName: {
             type = "app";
             program = "${package}/bin/${binaryName}";
@@ -129,6 +185,9 @@
               git-relay-ssh-force-command
               git-relay-service-templates;
             git-relay-service-render-check = serviceRenderCheck;
+            "rfc-proof-e2e-fast" = rfcProofFast;
+            "rfc-proof-e2e-full" = rfcProofFull;
+            "rfc-proof-provider-admission" = rfcProofProviderAdmission;
           };
 
           devShells = {
